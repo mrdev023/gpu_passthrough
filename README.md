6b1596f9e74dd9743d62089bb7d686f594274855
Florian RICHER <florian.richer@protonmail.com>
2023-02-05 20:19:13 +0100

    Add autoinstall scripts

---
+++ README.md
@@ -0,0 +1,17 @@
+Super wiki pour le GPU passthrough :
+https://wiki.archlinux.org/title/PCI_passthrough_via_OVMF
+
+Il explique assez bien lui et en FR ( Vidéo ):
+https://www.youtube.com/watch?v=CwEVj00SwYM
+
+Vidéo en anglais, pareil assez bien détaillée :
+https://www.youtube.com/watch?v=_JTEsQufSx4
+
+Ici tuto blog en FR :
+https://jugulaire.github.io/GPU-passthrough-KVM-QEMU/
+
+Wiki pas mal avec des fichiers pour les ROM GPU:
+https://gitlab.com/risingprismtv/single-gpu-passthrough/-/wikis/home
+
+Outil pour avoir une fenêtre windows dans Linux ( un peu comme VNC mais optimisé ) :
+https://github.com/gnif/LookingGlass
--- install.sh
+++ install.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 
-# Install dotfiles
-rm -rf "$HOME/.config/kvm"
-ln -s "$(pwd)/dotfiles/kvm" "$HOME/.config/kvm"
-echo "Symbolic link created : $(pwd)/dotfiles/kvm -> $HOME/.config/kvm"
+USERNAME='florian'
+SOURCE_FOLDER="$(pwd)/dotfiles/kvm"
+HOME_FOLDER="/home/$USERNAME"
+
+if [ $EUID -ne 0 ]; then
+	echo "This program must run as root to function." 
+	exit 1
+fi
+
+echo "This will install and configure libvirt."
+pacman -S libvirt libvirt-glib libvirt-python virt-install virt-manager qemu qemu-arch-extra ovmf vde2 dnsmasq bridge-utils openbsd-netcat swtpm --noconfirm
+
+echo "systemctl enable libvirtd"
+systemctl enable libvirtd
+
+echo "systemctl start libvirtd"
+systemctl start libvirtd
+
+echo "mv /etc/libvirt/libvirtd.conf /etc/libvirt/libvirtd.conf.old"
+mv /etc/libvirt/libvirtd.conf /etc/libvirt/libvirtd.conf.old
+
+echo "Adding $USERNAME to kvm and libvirt groups..."
+gpasswd -M $USERNAME kvm
+gpasswd -M $USERNAME libvirt
+
+echo "cp $SOURCE_FOLDER/libvirtd.conf /etc/libvirt"
+cp "$SOURCE_FOLDER/libvirtd.conf" /etc/libvirt
+echo "libvirt has been successfully configured!"
+
+echo "cp /etc/libvirt/qemu.conf /etc/libvirt/qemu.conf.old"
+cp /etc/libvirt/qemu.conf /etc/libvirt/qemu.conf.old
+
+echo "cp $SOURCE_FOLDER/libvirt/qemu.conf /etc/libvirt"
+cp $SOURCE_FOLDER/libvirt/qemu.conf /etc/libvirt
+
+echo "cp -R $SOURCE_FOLDER/libvirt/hooks /etc/libvirt"
+cp -R $SOURCE_FOLDER/libvirt/hooks /etc/libvirt
+
+echo "systemctl restart libvirtd"
+systemctl restart libvirtd
+
+echo "QEMU has been successfully configured!"
